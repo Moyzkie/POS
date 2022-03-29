733e812b78dc802461e2eee37bc82402a35338d1
Moyzkie <Rockyelicana@gmail.com>
2022-03-29 21:47:04 +0800

    first Commit

---
+++ application/views/Cashierpages/shop.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Login</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
+  <a class="navbar-brand" href="#">Shop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Setting
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<section>
+   <div class="container-fluid mt-2">
+    <div class ="row">
+     <div class="ml-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.foodbusinessnews.net/ext/resources/2020/10/KelloggProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+        <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+     <div class="m-2">
+       <div class="card" style="width: 11rem; height:14rem">
+          <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2021/1/KelloggSimpleMillsProducts_Lead.jpg" alt="Card image cap">
+          <div class="card-body">
+          <div class="col-lg-16">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                   <span class="fas fa-minus"></span>
+                </button>
+              </span>
+              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
+              <span class="input-group-btn">
+                <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                  <span class="fas fa-plus"></span>
+                </button>
+              </span>
+            </div>
+        </div>
+         <button class="btn btn-outline-primary form-control mt-1">Buy</button>
+        </div>
+      </div>
+     </div>
+    </div>
+   </div>
+</section>
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function(){
+
+var quantitiy=0;
+   $('.quantity-right-plus').click(function(e){
+        
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+            
+            $('#quantity').val(quantity + 1);
+
+          
+            // Increment
+        
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+      
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+    
+});
+</script>
